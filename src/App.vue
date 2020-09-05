3d8b3f7d235fbacc971ae5230b68a9fbaa6e8766
Sagar Patil <sagarreddypatil@gmail.com>
2020-09-04 23:18:12 -0400

    Create main landing page

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,29 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view />
+    <section class="hero is-success is-fullheight welcome">
+      <div class="hero-body">
+        <div class="container">
+          <h1 class="title">
+            Hi There!
+          </h1>
+          <h2 class="subtitle">
+            My name is Sagar Patil
+          </h2>
+        </div>
+      </div>
+    </section>
   </div>
 </template>
 
 <style lang="scss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: Ubuntu, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
 }
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+.welcome {
+  background-image: linear-gradient(135deg, #8bc6ec 0%, #9599e2 100%);
 }
 </style>
