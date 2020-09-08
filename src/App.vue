605302ffe25b7415fa402652ab97c93306ea23cf
Sagar Patil <sagarreddypatil@gmail.com>
2020-09-08 10:08:26 -0400

    I have no idea

--- src/App.vue
+++ src/App.vue
@@ -1,61 +1,25 @@
 <template>
   <div id="app">
-    <section class="hero is-success is-fullheight welcome">
-      <div class="hero-body">
-        <div class="container">
-          <div class="title">
-            Hi There!
-          </div>
-          <div class="subtitle">
-            My name is Sagar Patil
-          </div>
-        </div>
-      </div>
-      <div id="down-arrow">
-        <a href="#" id="chevron-down-button"
-          ><chevron-down-icon size="2x" class="mb-4"></chevron-down-icon
-        ></a>
-      </div>
-    </section>
+    <router-view />
   </div>
 </template>
 
-<script lang="ts">
-import Vue from "vue";
-import { ChevronDownIcon } from "vue-feather-icons";
-export default Vue.extend({
-  components: {
-    ChevronDownIcon
-  }
-});
-</script>
-
 <style lang="scss">
 #app {
-  font-family: Ubuntu, sans-serif;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
+  color: #2c3e50;
 }
-
-.welcome {
-  background-image: linear-gradient(135deg, #8bc6ec 0%, #9599e2 100%);
-}
-
-#chevron-down-button {
-  color: #ffffff;
-}
-
-#chevron-down-button :hover {
-  color: #ffffff;
-}
-
-#down-arrow {
-  position: relative;
-  top: 0;
-  transition: top ease 0.5s;
-}
-#down-arrow:hover {
-  top: -10px;
+#nav {
+  padding: 30px;
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,59 @@
 <template>
-  <div class="home">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
-  </div>
+  <section class="hero is-success is-fullheight welcome">
+    <div class="hero-body">
+      <div class="container">
+        <div class="title">
+          Hi There!
+        </div>
+        <div class="subtitle">
+          My name is Sagar Patil
+        </div>
+      </div>
+    </div>
+    <div id="down-arrow">
+      <a href="#" id="chevron-down-button"
+        ><chevron-down-icon size="2x" class="mb-4"></chevron-down-icon
+      ></a>
+    </div>
+  </section>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from "vue-property-decorator";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
-
-@Component({
+import Vue from "vue";
+import { ChevronDownIcon } from "vue-feather-icons";
+export default Vue.extend({
   components: {
-    HelloWorld
+    ChevronDownIcon
   }
-})
-export default class Home extends Vue {}
+});
 </script>
+
+<style lang="scss">
+#app {
+  font-family: Ubuntu, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+}
+
+.welcome {
+  background-image: linear-gradient(135deg, #8bc6ec 0%, #9599e2 100%);
+}
+
+#chevron-down-button {
+  color: #ffffff;
+}
+
+#chevron-down-button :hover {
+  color: #ffffff;
+}
+
+#down-arrow {
+  position: relative;
+  top: 0;
+  transition: top ease 0.5s;
+}
+#down-arrow:hover {
+  top: -10px;
+}
+</style>
