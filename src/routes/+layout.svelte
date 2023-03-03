1755f314ca819ea66443edb432d04c75eaa1fa6b
Sagar Patil <sagarreddypatil@gmail.com>
2023-03-03 04:06:45 -0500

    smal

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -8,8 +8,12 @@
 		<div class="flex items-center justify-between p-3">
 			<h1 class="text-5xl font-bold animate-slidein-left">Sagar Patil</h1>
 			<div class="text-4xl flex justify-end">
-				<i class="bi bi-github px-1" />
-				<i class="bi bi-linkedin px-1" />
+				<i
+					class="bi bi-github px-1 animate-[slidein-right_0.5s_ease-in-out_0.25s_normal_backwards]"
+				/>
+				<i
+					class="bi bi-linkedin px-1 animate-[slidein-right_0.5s_ease-in-out_0s_normal_backwards]"
+				/>
 			</div>
 		</div>
 		<hr class="my-4 animate-[reveal-left_1s_ease-in-out_normal_forwards]" />
