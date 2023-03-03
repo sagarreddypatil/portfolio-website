82cb841d556d9f2f554e8b94916eec099ea988f9
Sagar Patil <sagarreddypatil@gmail.com>
2023-03-03 03:30:00 -0500

    some foundation

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,44 @@
+<script>
+	import '../app.css';
+</script>
+
+<div class="bg-white dark:bg-black text-black dark:text-white h-screen overflow-hidden">
+	<div class="w-8/12 mx-auto">
+		<!-- Nav with name -->
+		<div class="flex items-center justify-between p-3">
+			<h1 class="text-6xl font-bold animate-in-left">Sagar Patil</h1>
+		</div>
+		<hr class="my-4 animate-open" />
+		<slot />
+	</div>
+</div>
+
+<style>
+	@keyframes open-left {
+		0% {
+			width: 0;
+		}
+		100% {
+			width: 100%;
+		}
+	}
+
+	@keyframes in-left {
+		0% {
+			opacity: 0;
+			transform: translateX(-10%);
+		}
+		100% {
+			opacity: 1;
+			transform: translateX(0);
+		}
+	}
+
+	.animate-open {
+		animation: open-left 0.5s ease-in-out;
+	}
+
+	.animate-in-left {
+		animation: in-left 0.25s ease-in-out;
+	}
+</style>
