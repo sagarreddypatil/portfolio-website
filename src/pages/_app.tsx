e8d14760821772af66fe352a5d1b94f7d2e06567
sagarreddypatil <sagarreddypatil@gmail.com>
2023-09-10 21:15:27 -0400

    analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,23 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
+import Script from "next/script";
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Script
+        strategy="lazyOnload"
+        src="https://www.googletagmanager.com/gtag/js?id=G-M3KTQVTXVG"
+      />
+      <Script strategy="lazyOnload">
+        {`window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-M3KTQVTXVG');`}
+      </Script>
+      <Component {...pageProps} />
+    </>
+  );
 }
