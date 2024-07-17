711c0dac230bde5daf11018245a86c990edc2622
Sagar Patil <sagarreddypatil@gmail.com>
2024-07-16 23:27:10 -0400

    fix ci?

--- build.sh
+++ build.sh
@@ -1,24 +1,29 @@
 #!/bin/sh
 
+fail() {
+    echo "Build failed"
+    exit 1
+}
+
 tw() {
     echo "Building Tailwind CSS to dist/index.css"
-    ./node_modules/.bin/tailwindcss -i ./src/index.css -o ./dist/index.css --minify
+    ./node_modules/.bin/tailwindcss -i ./src/index.css -o ./dist/index.css --minify || fail
 }
 
 html() {
     echo "Building HTML files to dist"
-    python src/build.py --output dist --no-clean
+    python src/build.py --output dist --no-clean || fail
 }
 
 static() {
     for ent in public/*; do
         echo "Copying $ent to dist/${ent##*/}"
-        cp -r $ent dist/${ent##*/}
+        cp -r $ent dist/${ent##*/} || fail
     done
 }
 
 opt_imgs() {
-    ./src/optimize-images.sh
+    ./src/optimize-images.sh || fail
 }
 
 html_static() {
